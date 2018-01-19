5990d973e669088268488f56717a8bae13c4f65a
nathanknautz <nate.knautz@gmail.com>
2018-01-18 18:26:36 -0600

    updated for body params

--- runner.rb
+++ runner.rb
@@ -5,12 +5,19 @@ while !solved
   system "clear"
   print "Guess: "
   input_guess = gets.chomp
-  response = Unirest.get("http://localhost:3000/segment_games/url/#{input_guess}")
+
+  response = Unirest.get(
+                        "http://localhost:3000/games_url",
+                        parameters: {
+                                      guess: input_guess
+                            }
+                            )
   data = response.body
 
   if data["answer"] == "You got it!"
     solved = true
   end 
+
   puts JSON.pretty_generate(data)
   x = gets.chomp
 end 
